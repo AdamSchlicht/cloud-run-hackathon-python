56d761fa835a315667efab6257222bf24e628abc
Adam Schlicht <aschlicht@student.unimelb.edu.au>
2022-08-26 14:10:53 +1000

    Update main.py

--- main.py
+++ main.py
@@ -39,11 +39,4 @@ if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
 
 
-  
-def test_function(test_variable):
-    test_variable = test_variable + 1
-
-    return test_variable
-
-
 
